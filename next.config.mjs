702d1ddb3e9f2769560714e01a4f21a1f4807e0f
bedcomfy <166274190+bedcomfy@users.noreply.github.com>
2024-08-18 00:07:52 +0000

    webpack changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        usb: false,
+        noble: false,
+      };
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
